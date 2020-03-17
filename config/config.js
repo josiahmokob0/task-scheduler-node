30f33bdb4e57539a4e17a2b3bdcbb2e73cbbd0d7
hjkl <jmnyarega@gmail.com>
2020-03-17 18:55:04 +0300

    change database url

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ module.exports = {
     dialect: process.env.TEST_DB_DIALECT,
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: process.env.PROD_DB_URL,
     dialect: process.env.PROD_DB_DIALECT,
     dialectOptions: {
       ssl: true
